e33653d47c62454943bd8932ea4d672df10f4098
Alzaheer Khan K <alzaheerkhan7@gmail.com>
2026-04-19 22:16:58 +0530

    commit changes

---
+++ Admin_app_lib_folder/utility/constants.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+const String MAIN_URL = "http://172.16.31.87:3000";
+
+
+const primaryColor = Color(0xFF2697FF);
+const secondaryColor = Color(0xFF2A2D3E);
+const bgColor = Color(0xFF212332);
+const defaultPadding = 16.0;
+
+
