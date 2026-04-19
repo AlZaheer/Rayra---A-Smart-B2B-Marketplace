fee5e09f26a5288bc7dd2492606df495e66ed38c
Alzaheer Khan K <alzaheerkhan7@gmail.com>
2026-04-19 21:42:41 +0530

    Add files via upload

---
+++ lib/utility/app_color.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart' show Color;
+
+class AppColor {
+  const AppColor._();
+
+  static const darkOrange = Color.fromARGB(255, 38, 6, 224);
+  static const lightOrange = Color.fromARGB(255, 212, 10, 223);
+
+  static const darkGrey = Color(0xFFA6A3A0);
+  static const lightGrey = Color(0xFFE5E6E8);
+}
